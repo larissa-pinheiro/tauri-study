c8b009b9e3d54c53cda6ab4e91d0f256d66d65c3
Larissa <rissapins@gmail.com>
2024-07-02 20:27:47 -0300

    feat: add cabeçalho personalizado

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ export default function App() {
   return (
     <>
       <TitleMenu />
-      <h2>Outro</h2>
+      <h2 className="">Texto</h2>
+
+      <div className="flex justify-end w-4 h-4 rounded-full bg-slate-100" />
+
     </>
   )
 }
--- src/components/TitleMenu/TitleMenu.jsx
+++ src/components/TitleMenu/TitleMenu.jsx
@@ -1,65 +1,54 @@
-// para que funcione, é necessário instalar a api -> npm install @tauri-apps/api
-import { appWindow } from '@tauri-apps/api/window';
-import React from 'react';
+import { appWindow } from '@tauri-apps/api/window'; // para que funcione, é necessário instalar a api -> npm install @tauri-apps/api
+
+import { Minus, Square, X } from "@phosphor-icons/react";
 
 export default function TitleMenu() {
+  const handleDragWindow = () => {
+    appWindow.startDragging();
+  };
+
   const handleMinimize = () => {
     appWindow.minimize();
   };
 
-  const handleMaximize = () => {
-    appWindow.isMaximized().then((isMaximized) => {
-      if (isMaximized) {
-        appWindow.unmaximize();
-      } else {
-        appWindow.maximize();
-      }
-    });
+  const handleMaximize = async () => {
+    await appWindow.toggleMaximize();
+
   };
 
-  async function handleDragWindow() {
-    // Apenas iniciar o arrasto se for o botão esquerdo do mouse
-    if (e.button === 0) {
-      appWindow.startDragging();
-    }
-  }
+  const handleClose = () => {
+    appWindow.close();
+  };
+
+  const buttonClass = "hover:bg-gray-700 text-gray-400 hover:text-white py-2 px-3 transition-colors duration-200";
 
   return (
-    <div onMouseDown={handleDragWindow}
-      className="bg-gray-800 text-white flex justify-end px-4 py-2">
+    <div
+      className="bg-slate-600 flex justify-end">
+
+      <div
+        className="flex w-full"
+        onMouseDown={handleDragWindow}
+      >
+        <h1 className='flex select-none items-center ps-3'>Tauri Study</h1>
+      </div>
       <button
         onClick={handleMinimize}
-        className="bg-gray-600 hover:bg-gray-700 rounded-full p-1 mx-1"
+        className={buttonClass}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-4 w-4"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M5 10a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          />
-        </svg>
+        <Minus size={12} />
       </button>
       <button
         onClick={handleMaximize}
-        className="bg-gray-600 hover:bg-gray-700 rounded-full p-1 mx-1"
+        className={buttonClass}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-4 w-4"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 6a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 6a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          />
-        </svg>
+        <Square />
+      </button>
+      <button
+        onClick={handleClose}
+        className="hover:bg-red-600 text-gray-400 hover:text-white py-2 px-3 transition-colors duration-200"
+      >
+        <X />
       </button>
     </div>
   );
